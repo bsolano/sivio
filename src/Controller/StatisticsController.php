d77d0f02ab1c4c07fc32feba6e2b41f49ae5a5ef
edsv <eduardosv5@hotmail.com>
2016-05-24 21:33:00 +0000

    Modificado formulario de estadísticas, cambios en las consultas.

--- src/Controller/StatisticsController.php
+++ src/Controller/StatisticsController.php
@@ -22,49 +22,87 @@ class StatisticsController extends AppController
         $statistic = $this->Statistics->newEntity();
         if ($this->request->is('post')) {
             
-           //acá va el código para las consultas
+            // Código de las consultas
            
             $s = $this->request->data;
+            
+            // Carga el arreglo de datos
+            
             $data = array($s);
             
-           $nacionalidad = $data[0]['nacionalidad'];
-           $edad = $data[0]['edad'];
-           $escolaridad = $data[0]['escolaridad'];
-           $estado_civil = $data[0]['estado_civil'];
-           $ocupacion = $data[0]['ocupacion'];
-           //print_r(array_filter($data[0]));
-           //echo $edad;
+            // Saca los datos de los campos de texto en los que se escribió
+            
+            $nacionalidad = $data[0]['nacionalidad'];
+            // $edad = $data[0]['edad'];
+            $edadLow = $data[0]['edadLower']; // Lower bound de la edad
+            $edadUp = $data[0]['edadUpper']; // Upper bound de la edad
+            $escolaridad = $data[0]['escolaridad'];
+            $estado_civil = $data[0]['estado_civil'];
+            $ocupacion = $data[0]['ocupacion'];
+            
+            //print_r(array_filter($data[0]));
+            //echo $edad;
            
-            if ($data[0] != NULL) {
+            if ($data[0] != NULL) { // Si se ingresó algo en los campos
                 
-                //se declaran los criterios de búsqueda
+                /*          POR AHORA SE DEJA ESTO COMENTADO PARA HACER LA CONSULTA DE OTRA MANERA
+                
+                // Se declaran criterios de búsqueda para las consultas
                 
                 $arreglo = array('People.edad >=' => '1','People.nacionalidad' => '' );
                 
-                //elimina los campos en blanco del query
+                // Elimina los campos en blanco del query
+                
                 $array = array_filter($arreglo);
                 
-               $opciones=array('conditions'=> (array('People.edad >=' => '0','People.nacionalidad' => '' )));
+                $opciones=array('conditions'=> (array('People.edad >=' => '0','People.nacionalidad' => '' )));
                
-               print_r(($array));
-           
-               //se construye el query
-	          $query = $this->People->find('all',$opciones);
-	           
-	           //se recorre el resultado de la consulta
-                foreach ($query as $row) {
+                print_r(($array));
+                
+                */
+                
+                //$opciones = array('conditions' => array('Personas.nombre' => "David"));
+                
+                // Establece criterios de consulta
+                
+                /*
+                
+                $opciones = $this->People->find('all',array('conditions'=>array(array(
+                    'People.edad BETWEEN ? AND ?' => array($edadLow,$edadUp)))));
+                
+                */
+                
+                /*              PROPUESTAS DE CONSULTAS
+                
+                $opciones = array(find('all', array('conditions'=>
+                    array('People.edad >=' => 10,
+                         'People.edad <=' => 20)
+                    )));
+                    
+                
+                */
+                
+                // Ejecuta consulta    
+                    
+                // $personasEncontradas = $this->People->find('all', $opciones);
+                
+	            // Se recorre el resultado de la consulta
+	            
+                //foreach ($personasEncontradas as $row) {
                     //echo $row->nombre.' -> '.$row->nacionalidad;
                 
-                }
+                //}
+                
+                
                 
                 //,array('conditions'=>array(array('People.edad BETWEEN ? AND ?' => array(15,45))))
                 //int_r($personasFiltradas);
                 
-                $this->Flash->success(__('The person has been saved.'));
+                $this->Flash->success(__('Éxito en consulta de estadísticas'));
                 //echo $statistic;
                 //return $this->redirect(['action' => 'index']);
             } else {
-                $this->Flash->error(__('Please, try again.'));
+                $this->Flash->error(__('Error, pruebe de nuevo.'));
             }
         }
          //$this->set('statistic',$m);
@@ -84,5 +122,8 @@ class StatisticsController extends AppController
     
     
     
+        
+            
+
     
 }
--- src/Template/Statistics/index.ctp
+++ src/Template/Statistics/index.ctp
@@ -3,24 +3,41 @@
     <fieldset>
      
         
-            <legend><?= __('Reporte de Estadísticas') ?></legend>
-            <?php
-           echo $this->Form->input('nacionalidad',['empty' => true,'label' => 'Nacionalidad' ]);
-           echo $this->Form->input('ocupacion',['empty' => true,'label' => 'Ocupación' ]);
-           echo 'Estado Civil';
+        <legend><?= __('Reporte de Estadísticas') ?></legend>
+        <?php
+            echo $this->Form->input('nacionalidad',['empty' => true, 'label' => 'Nacionalidad' ]);
+            echo $this->Form->input('ocupacion',['empty' => true, 'label' => 'Ocupación' ]);
+            echo 'Estado Civil';
             $options = ['soltera' => 'Soltera', 'casada' => 'Casada', 'union libre' => 'Unión libre','soltera' => 'Soltera','divorciada' => 'Divorciada','separada' => 'Separada'];
-            echo $this->Form->select('estado_civil', $options,['value' => 0]);
+            echo $this->Form->select('estado_civil', $options, ['value' => 0]);
             echo 'Escolaridad';
             $options = ['ningún grado' => 'Ningún grado','primaria completa' => 'Primaria Completa', 'primaria incompleta' => 'Primaria incompleta', 'secundaria completa' => 'Secundaria completa','secundaria incompleta' => 'Secundaria incompleta','parauniversitario/técnico' => 'Parauniversitario/técnico','universitaria completa' => 'Universitaria completa','universitaria incompleta' => 'Universitaria incompleta'];
             echo $this->Form->select('escolaridad', $options,['value' => 0]);
             
-            echo 'Edad';
-            $options = ['0' => '', '10' => '0-10', '20' => '11-20', '30' => '21-30'];
-            echo $this->Form->select('edad', $options,['value' => 0 ,'label' => 'Edad']);
+            echo 'Edad entre: '; 
+        ?>
+            
+        <div class = "row">
+            <div class = "large-6 medium-6 columns">
+                <?php echo $this->Form->input('edadLower', ['empty' => true, 'label' => 'Entre']); ?>
+            </div>
+            
+            <div class = "large-6 medium-6 columns">
+                <?php echo $this->Form->input('edadUpper', ['empty' => true, 'label' => 'y']); ?>
+            </div> 
+        </div> <!--/ First row dates-->
+               
+        <?php
+        
+        // El lower bound queda almacenado en edadLower
+        // El upper bound queda almacenado en edadUpper
+        
+        //$options = ['0' => '', '10' => '0-10', '20' => '11-20', '30' => '21-30'];
+        //echo $this->Form->select('edad', $options,['value' => 0 ,'label' => 'Edad']);
             
             
-             ?>
-             
+        ?>
+        
     </fieldset>
     <?= $this->Form->button(__('Submit')) ?>
     <?= $this->Form->end() ?>
